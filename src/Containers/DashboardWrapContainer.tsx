3c7a74c181fb238637ee0d2cd1c6099cefb5c7c6
spearker <scchosam132@gmail.com>
2020-10-24 18:58:21 +0900

    통계 전력 디자인 이슈 해결

--- src/Containers/DashboardWrapContainer.tsx
+++ src/Containers/DashboardWrapContainer.tsx
@@ -1,89 +1,89 @@
-import React, {useEffect} from 'react';
+import React, {useEffect} from 'react'
 import Styled from 'styled-components'
 import {
-    BG_COLOR,
-    BG_COLOR_SUB,
-    SYSTEM_NAME,
-    BG_COLOR_SUB2,
-    COMPANY_LOGO,
-    POINT_COLOR,
-    MAX_WIDTH,
-    BASE_URL,
-    TOKEN_NAME
+  BG_COLOR,
+  BG_COLOR_SUB,
+  SYSTEM_NAME,
+  BG_COLOR_SUB2,
+  COMPANY_LOGO,
+  POINT_COLOR,
+  MAX_WIDTH,
+  BASE_URL,
+  TOKEN_NAME
 } from '../Common/configset'
 import DashboardNavigation from '../Components/Navigation/DashboardNavigation'
-import ProfileBar from '../Components/Navigation/ProfileBar';
-import {getToken, setToken, loadXHR} from '../Common/tokenFunctions';
-import {useUserDispatch, useUser} from '../Context/UserContext';
-import {getRequest} from '../Common/requestFunctions';
-import {PM_MENU_LIST} from '../Common/routerset';
+import ProfileBar from '../Components/Navigation/ProfileBar'
+import {getToken, setToken, loadXHR} from '../Common/tokenFunctions'
+import {useUserDispatch, useUser} from '../Context/UserContext'
+import {getRequest} from '../Common/requestFunctions'
+import {PM_MENU_LIST} from '../Common/routerset'
 
 //대시보드를 감싸는 wrap 박스
 
 const DashboardWrapContainer = ({children, index}: any) => {
 
-    const dispatch = useUserDispatch();
-    const User = useUser();
-    /**
-     * loadUserInfo()
-     * : 유저 정보 로드 후 user info dispatch
-     * @returns X
-     */
-    const loadUserInfo = async () => {
-        if (User.pk !== "") {
-            return;
-        }
-        const results = await getRequest('http://203.234.183.22:8299/api/v1/user/load', getToken(TOKEN_NAME))
-
-        if (results === false) {
-            //TODO: 에러 처리
-        } else {
-            if (results.status === 200) {
-                dispatch({
-                    type: 'SET_USER',
-                    data: {
-                        pk: results.results.pk,
-                        email: results.results.email,
-                        is_admin: results.results.is_admin,
-                        appointment: results.results.appointment,
-                        name: results.results.name,
-                        profile_img: results.results.profile_img,
-                        is_login: true,
-                        company_name: results.results.company_name,
-                    }
-                });
-
-                loadXHR(results.results.profile_img).then(function (blob) {
-                    setToken('sizl_photo', blob)
-                })
-            } else {
-                //TODO : 지울것
-                //alert('세션 체크 실패 : 테스트 기간동안은 임시로 비로그인 접속 허용')
-            }
-        }
+  const dispatch = useUserDispatch()
+  const User = useUser()
+  /**
+   * loadUserInfo()
+   * : 유저 정보 로드 후 user info dispatch
+   * @returns X
+   */
+  const loadUserInfo = async () => {
+    if (User.pk !== '') {
+      return
     }
+    const results = await getRequest('http://61.101.55.224:18299/api/v1/user/load', getToken(TOKEN_NAME))
 
-    useEffect(() => {
+    if (results === false) {
+      //TODO: 에러 처리
+    } else {
+      if (results.status === 200) {
+        dispatch({
+          type: 'SET_USER',
+          data: {
+            pk: results.results.pk,
+            email: results.results.email,
+            is_admin: results.results.is_admin,
+            appointment: results.results.appointment,
+            name: results.results.name,
+            profile_img: results.results.profile_img,
+            is_login: true,
+            company_name: results.results.company_name,
+          }
+        })
 
-        loadUserInfo();
+        loadXHR(results.results.profile_img).then(function (blob) {
+          setToken('sizl_photo', blob)
+        })
+      } else {
+        //TODO : 지울것
+        //alert('세션 체크 실패 : 테스트 기간동안은 임시로 비로그인 접속 허용')
+      }
+    }
+  }
 
-    }, [])
+  useEffect(() => {
+
+    loadUserInfo()
+
+  }, [])
 
 
-    return (
-        <>
-            <DashboardWrapDiv>
-                <DashboardNavigation select={index}/>
-                <div style={{width: '100%', marginBottom: 88, textAlign: 'center'}}>
-                    <ProfileBar title={Object.keys(PM_MENU_LIST).find(f => f == index) ? 'PM System' : 'MES System'}/>
-                    <div style={{minWidth: 1100}}>
-                        {children}
-                    </div>
-                </div>
-            </DashboardWrapDiv>
-        </>
+  return (
+    <>
+      <DashboardWrapDiv>
+        <DashboardNavigation select={index}/>
+        <div style={{width: '100%', marginBottom: 88, textAlign: 'center'}}>
+          <ProfileBar title={Object.keys(PM_MENU_LIST).find(f => f == index) ? 'PM System' : 'MES System'}/>
+          <div style={{minWidth: 1100}}>
+            {children}
+          </div>
+        </div>
+      </DashboardWrapDiv>
+    </>
 
-    );
+  )
 }
 
 const DashboardWrapDiv = Styled.div`
@@ -96,4 +96,4 @@ const DashboardWrapDiv = Styled.div`
     position: relative;
 `
 
-export default DashboardWrapContainer;
+export default DashboardWrapContainer
--- src/Pages/OldMaintenance/History.tsx
+++ src/Pages/OldMaintenance/History.tsx
@@ -1,153 +1,153 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react'
 import Styled from 'styled-components'
 import {BG_COLOR_SUB2, TOKEN_NAME} from '../../Common/configset'
-import DashboardWrapContainer from '../../Containers/DashboardWrapContainer';
-import Header from '../../Components/Text/Header';
-import {getToken} from '../../Common/tokenFunctions';
+import DashboardWrapContainer from '../../Containers/DashboardWrapContainer'
+import Header from '../../Components/Text/Header'
+import {getToken} from '../../Common/tokenFunctions'
 import 'react-dropdown/style.css'
 import {dataSet} from '../../Common/dataset'
-import SubNavigation from '../../Components/Navigation/SubNavigation';
-import {ROUTER_MENU_LIST} from '../../Common/routerset';
-import InnerBodyContainer from '../../Containers/InnerBodyContainer';
-import {getRequest, postRequest} from '../../Common/requestFunctions';
-import SearchInputSmall from '../../Components/Input/SearchInputSmall';
+import SubNavigation from '../../Components/Navigation/SubNavigation'
+import {ROUTER_MENU_LIST} from '../../Common/routerset'
+import InnerBodyContainer from '../../Containers/InnerBodyContainer'
+import {getRequest, postRequest} from '../../Common/requestFunctions'
+import SearchInputSmall from '../../Components/Input/SearchInputSmall'
 import TEMP_IMG_1 from '../../Assets/Dummy/repair_history.svg'
 
 const MaintenanceHistory = () => {
 
-    const [list, setList] = useState<IMaintenance[]>([]);
-    const [option, setOption] = useState(0);
-    const [keyword, setKeyword] = useState<string>('');
-    const type = "machine"
-    const optionList = [
-        "등록순", "이름순"
-    ]
-    const index = {
-        name: '이름',
+  const [list, setList] = useState<IMaintenance[]>([])
+  const [option, setOption] = useState(0)
+  const [keyword, setKeyword] = useState<string>('')
+  const type = 'machine'
+  const optionList = [
+    '등록순', '이름순'
+  ]
+  const index = {
+    name: '이름',
+  }
+
+  /**
+   * getSearchList()
+   * 목록 검색
+   * @param {string} url
+   * @returns X
+   */
+  const getSearchList = useCallback(async (e) => {
+    e.preventDefault()
+    const results = await getRequest('http://61.101.55.224:18299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
+
+    if (results === false) {
+      ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+    } else {
+      if (results.status === 200) {
+        setList(results.results)
+        setKeyword('')
+      } else {
+        ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+      }
+    }
+  }, [list, option, keyword, type])
+
+  /**
+   * getList()
+   * 목록 불러오기
+   * @param {string} url
+   * @returns X
+   */
+  const getList = useCallback(async () => {
+
+    const results = await getRequest('http://61.101.55.224:18299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
+    if (results === false) {
+      ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+    } else {
+      if (results.status === 200) {
+        setList(results.results)
+      } else {
+        ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+      }
+    }
+  }, [option, keyword, list])
+
+  /**
+   * onClickFilter()
+   * 리스트 필터 변경
+   * @param {string} filter 필터 값
+   * @returns X
+   */
+  const onClickFilter = useCallback(async (filter: number) => {
+    setOption(filter)
+    ////alert(`선택 테스트 : 필터선택 - filter : ${filter}` )
+
+    const results = await getRequest('http://61.101.55.224:18299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
+    if (results === false) {
+      ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+    } else {
+      if (results.status === 200) {
+        setList(results.results)
+      } else {
+        ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
+      }
+    }
+  }, [option, keyword, list])
+
+  useEffect(() => {
+    //getList()
+
+    setList(dataSet.maintenanceList)
+  }, [])
+  const onClickModify = useCallback((id) => {
+
+    console.log('--select id : ' + id)
+    window.location.href = `/update/design?pk=${id}`
+
+  }, [])
+  const onClickDelete = useCallback(async (id) => {
+
+    const results = await postRequest('http://61.101.55.224:18299/api/v1/preserve/delete', {pk: id}, getToken(TOKEN_NAME))
+
+    console.log('--select id : ' + id)
+    if (results === false) {
+      //alert('요청을 처리 할 수없습니다. 잠시후 다시 이용하세요.')
+    } else {
+      if (results.status === 200) {
+        getList()
+      } else {
+        //alert('요청을 처리 할 수없습니다. 잠시후 다시 이용하세요.')
+      }
     }
 
-    /**
-     * getSearchList()
-     * 목록 검색
-     * @param {string} url
-     * @returns X
-     */
-    const getSearchList = useCallback(async (e) => {
-        e.preventDefault();
-        const results = await getRequest('http://203.234.183.22:8299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
 
-        if (results === false) {
-            ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-        } else {
-            if (results.status === 200) {
-                setList(results.results)
-                setKeyword('')
-            } else {
-                ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-            }
-        }
-    }, [list, option, keyword, type])
+  }, [])
+  return (
+    <DashboardWrapContainer index={5}>
+      <SubNavigation list={ROUTER_MENU_LIST[5]}/>
+      <InnerBodyContainer>
+        <div style={{position: 'relative'}}>
+          <Header title={`보전 이력`}/>
+          <div style={{position: 'absolute', display: 'inline-block', top: 0, right: 0}}>
+            <SearchInputSmall
+              description={'검색어 입력'}
+              value={keyword}
+              onChangeEvent={(e) => {
+                setKeyword(e.target.value)
+              }}
+              onClickEvent={() => {
+              }}
+            />
+          </div>
+        </div>
 
-    /**
-     * getList()
-     * 목록 불러오기
-     * @param {string} url
-     * @returns X
-     */
-    const getList = useCallback(async () => {
+        <WrapBox>
 
-        const results = await getRequest('http://203.234.183.22:8299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
-        if (results === false) {
-            ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-        } else {
-            if (results.status === 200) {
-                setList(results.results)
-            } else {
-                ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-            }
-        }
-    }, [option, keyword, list])
+          <img src={TEMP_IMG_1}/>
 
-    /**
-     * onClickFilter()
-     * 리스트 필터 변경
-     * @param {string} filter 필터 값
-     * @returns X
-     */
-    const onClickFilter = useCallback(async (filter: number) => {
-        setOption(filter)
-        ////alert(`선택 테스트 : 필터선택 - filter : ${filter}` )
-
-        const results = await getRequest('http://203.234.183.22:8299/api/v1/preserve/list?keyword=' + keyword + '&option=' + option + '&type=' + type, getToken(TOKEN_NAME))
-        if (results === false) {
-            ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-        } else {
-            if (results.status === 200) {
-                setList(results.results)
-            } else {
-                ////alert('데이터를 불러 올 수 없습니다. 잠시후 이용하세요.')
-            }
-        }
-    }, [option, keyword, list])
-
-    useEffect(() => {
-        //getList()
-
-        setList(dataSet.maintenanceList)
-    }, [])
-    const onClickModify = useCallback((id) => {
-
-        console.log('--select id : ' + id)
-        window.location.href = `/update/design?pk=${id}`
-
-    }, [])
-    const onClickDelete = useCallback(async (id) => {
-
-        const results = await postRequest('http://203.234.183.22:8299/api/v1/preserve/delete', {pk: id}, getToken(TOKEN_NAME))
-
-        console.log('--select id : ' + id)
-        if (results === false) {
-            //alert('요청을 처리 할 수없습니다. 잠시후 다시 이용하세요.')
-        } else {
-            if (results.status === 200) {
-                getList()
-            } else {
-                //alert('요청을 처리 할 수없습니다. 잠시후 다시 이용하세요.')
-            }
-        }
+        </WrapBox>
 
 
-    }, [])
-    return (
-        <DashboardWrapContainer index={5}>
-            <SubNavigation list={ROUTER_MENU_LIST[5]}/>
-            <InnerBodyContainer>
-                <div style={{position: 'relative'}}>
-                    <Header title={`보전 이력`}/>
-                    <div style={{position: 'absolute', display: 'inline-block', top: 0, right: 0}}>
-                        <SearchInputSmall
-                            description={'검색어 입력'}
-                            value={keyword}
-                            onChangeEvent={(e) => {
-                                setKeyword(e.target.value)
-                            }}
-                            onClickEvent={() => {
-                            }}
-                        />
-                    </div>
-                </div>
+      </InnerBodyContainer>
+    </DashboardWrapContainer>
 
-                <WrapBox>
-
-                    <img src={TEMP_IMG_1}/>
-
-                </WrapBox>
-
-
-            </InnerBodyContainer>
-        </DashboardWrapContainer>
-
-    );
+  )
 }
 const FullPageDiv = Styled.div`
   width: 100%;
@@ -166,4 +166,4 @@ const WrapBox = Styled.div`
     }
 `
 
-export default MaintenanceHistory;
+export default MaintenanceHistory
