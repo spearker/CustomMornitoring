efae980b73e547bca11d9bacff72fe8aa1ff6e56
JungDonggun <donggun.dev@gmail.com>
2020-10-26 18:20:20 +0900

    djqept

--- src/Containers/Custom/dashboard/CustomSPMMotorAngulargaugeChart.tsx
+++ src/Containers/Custom/dashboard/CustomSPMMotorAngulargaugeChart.tsx
@@ -9,24 +9,23 @@ interface Props {
   value: number | undefined
 }
 
-const CustomSlideMotorAngulargaugeChart: React.FunctionComponent<Props> = ({ value }) => {
+const CustomSPMMotorAngulargaugeChart: React.FunctionComponent<Props> = ({ value }) => {
   return (
       <div>
         <div style={{ marginBottom: 20 }}>
-          <p style={{ textAlign: 'center', fontSize: 45, fontWeight: 'bold' }}>슬라이더 모터 부하량</p>
+          <p style={{ textAlign: 'center', fontSize: 48, fontWeight: 'bold' }}>SPM</p>
         </div>
         <GaugeChart
-            id="slide-chart"
+            id="spm-chart"
             colors={[ 'rgba(255, 255, 255, .7)' ]}
             nrOfLevels={25}
-            formatTextValue={() => ''}
             percent={value ? value : 0}
+            formatTextValue={(value) => value}
             needleColor={'rgba(255, 255, 255, 1)'}
             needleBaseColor={'rgba(255, 255, 255, 1)'}
             style={chartStyle}/>
-        <p style={{ textAlign: 'center', fontSize: 48, fontWeight: 'bold' }}>{value}</p>
       </div>
   )
 }
 
-export default CustomSlideMotorAngulargaugeChart
+export default CustomSPMMotorAngulargaugeChart
