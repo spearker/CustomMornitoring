46ee64cd4ea207187fc1d09730cc63a85a9b2386
MacBook_Pro <jmyoon555@gmail.com>
2020-11-06 02:14:19 +0900

    아이피 변경

--- src/Api/configs/basic.ts
+++ src/Api/configs/basic.ts
@@ -15,38 +15,38 @@ client.defaults.baseURL = 'http://112.168.150.239:8299/api'
 client.defaults.headers.common['Authorization'] = getToken(TOKEN_NAME)
 
 client.interceptors.response.use(function (response) {
-  console.log(response.data)
-  const returnError = getErrorCase(response.data.status)
+    console.log(response.data)
+    const returnError = getErrorCase(response.data.status)
 
-  if (returnError) {
-    return Promise.reject(returnError)
-  } else {
-    return response.data
-  }
+    if (returnError) {
+        return Promise.reject(returnError)
+    } else {
+        return response.data
+    }
 
 }, function (error) {
-  console.error(error)
-  // alert('[ERROR] 요청을 처리 할 수 없습니다.')
-  return Promise.reject(error)
+    console.error(error)
+    // alert('[ERROR] 요청을 처리 할 수 없습니다.')
+    return Promise.reject(error)
 
 })
 
 const getErrorCase = (code) => {
-  console.log(code)
-  switch (code) {
-    case 2000:
-      // alert('[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다')
-      return '[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다'
-    case 3000:
-      return '[조회 불가] 해당 ID를 지닌 데이터가 없습니다'
-    case 1011:
-      return '[삭제 불가] 해당 데이터를 참조하는 표준 문서가 존재합니다'
-    case 200:
-      return false
-    case '200':
-      return false
-    default:
-      return '[RESPONSE ERROR] 요청을 처리 할 수 없습니다.'
-  }
+    console.log(code)
+    switch (code) {
+        case 2000:
+            // alert('[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다')
+            return '[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다'
+        case 3000:
+            return '[조회 불가] 해당 ID를 지닌 데이터가 없습니다'
+        case 1011:
+            return '[삭제 불가] 해당 데이터를 참조하는 표준 문서가 존재합니다'
+        case 200:
+            return false
+        case '200':
+            return false
+        default:
+            return '[RESPONSE ERROR] 요청을 처리 할 수 없습니다.'
+    }
 }
 export default client
--- src/Api/configs/map.ts
+++ src/Api/configs/map.ts
@@ -15,38 +15,38 @@ client.defaults.baseURL = 'http://112.168.150.239:8299/api'
 client.defaults.headers.common['Authorization'] = getToken(TOKEN_NAME)
 
 client.interceptors.response.use(function (response) {
-  //console.log(response.data.status)
-  const returnError = getErrorCase(response.data.status)
+    //console.log(response.data.status)
+    const returnError = getErrorCase(response.data.status)
 
-  if (returnError) {
-    alert(returnError)
-    return Promise.reject()
-  } else {
-    return response.data
-  }
+    if (returnError) {
+        alert(returnError)
+        return Promise.reject()
+    } else {
+        return response.data
+    }
 
 }, function (error) {
-  console.error(error)
-  //alert('SERVER ERROR] 요청을 처리 할 수 없습니다.');
-  return Promise.reject(error)
+    console.error(error)
+    //alert('SERVER ERROR] 요청을 처리 할 수 없습니다.');
+    return Promise.reject(error)
 
 })
 
 const getErrorCase = (code: any) => {
-  switch (code) {
-    case 2000:
-      // alert('[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다')
-      return '[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다'
-    case 3000:
-      return '[삭제 불가] 기존 지도 데이터가 존재합니다. 기존 데이터를 수정해주세요.'
-    case 1011:
-      return '[삭제 불가] 해당 데이터를 참조하는 표준 문서가 존재합니다'
-    case 200:
-      return false
-    case '200':
-      return false
-    default:
-      return '[RESPONSE ERROR] 요청을 처리 할 수 없습니다.'
-  }
+    switch (code) {
+        case 2000:
+            // alert('[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다')
+            return '[삭제 불가] 해당 데이터를 참조하는 데이터가 있습니다'
+        case 3000:
+            return '[삭제 불가] 기존 지도 데이터가 존재합니다. 기존 데이터를 수정해주세요.'
+        case 1011:
+            return '[삭제 불가] 해당 데이터를 참조하는 표준 문서가 존재합니다'
+        case 200:
+            return false
+        case '200':
+            return false
+        default:
+            return '[RESPONSE ERROR] 요청을 처리 할 수 없습니다.'
+    }
 }
 export default client
