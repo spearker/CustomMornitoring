5ea2f995e566e8350c5f3b16d11ec3bc529eae91
MacBook_Pro <jmyoon555@gmail.com>
2020-11-10 19:45:00 +0900

    유동아이피 변경

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,2 +1,2 @@
-export const SF_ENDPOINT = 'http://106.240.250.74:8299'
-export const SF_ENDPOINT_ADMIN = 'http://106.240.250.74:8286/api'
+export const SF_ENDPOINT = 'http://112.168.150.239:8299'
+export const SF_ENDPOINT_ADMIN = 'http://112.168.150.239:8286/api'
