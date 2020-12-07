cbca4e927840d5b1cc208e9fd01b2d0d8f9a298d
MacBook_Pro <jmyoon555@gmail.com>
2020-12-07 20:30:47 +0900

    패치

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,4 +1,5 @@
 const SF_ADDRESS = '192.168.0.104'
 export const SF_ENDPOINT = `http://${SF_ADDRESS}:8299`
+export const SF_ENDPOINT_EXCEL = `http://${SF_ADDRESS}:8399`
 export const SF_ENDPOINT_ADMIN = `http://${SF_ADDRESS}:8286/api`
 export const SF_ENDPOINT_FILE = `http://${SF_ADDRESS}:8099`
