c575a44ed5dba805331912813e14d7a08e0228f3
spearker <scchosam132@gmail.com>
2020-12-07 14:34:03 +0900

    인사관리 수정 수정

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,3 +1,4 @@
-export const SF_ENDPOINT = 'http://203.234.183.22:8299'
-export const SF_ENDPOINT_ADMIN = 'http://203.234.183.22:8286/api'
-export const SF_ENDPOINT_FILE = 'http://203.234.183.22:8099'
+const SF_ADDRESS = '203.234.183.22'
+export const SF_ENDPOINT = `http://${SF_ADDRESS}:8299`
+export const SF_ENDPOINT_ADMIN = `http://${SF_ADDRESS}:8286/api`
+export const SF_ENDPOINT_FILE = `http://${SF_ADDRESS}:8099`
