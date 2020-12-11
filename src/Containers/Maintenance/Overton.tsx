c77e9d78603bb3f67ebebaa2d8ef2f19e6e54e72
spearker <scchosam132@gmail.com>
2020-12-11 10:25:26 +0900

    오버톤 페이지네이션 수정 ㅓ

--- src/Containers/Maintenance/Overton.tsx
+++ src/Containers/Maintenance/Overton.tsx
@@ -3,140 +3,141 @@ import OvertonTable from '../../Components/Table/OvertonTable'
 import LineTable from '../../Components/Table/LineTable'
 import {API_URLS, getOvertoneData} from '../../Api/pm/preservation'
 import Styled from 'styled-components'
-import Notiflix from "notiflix";
+import Notiflix from 'notiflix'
 
-Notiflix.Loading.Init({svgColor: "#1cb9df",});
+Notiflix.Loading.Init({svgColor: '#1cb9df',})
 
 
 const OvertonMaintenanceContainer = () => {
 
-    const [list, setList] = useState<any[]>([])
-    const [detailList, setDetailList] = useState<any[]>([])
-    const [option, setOption] = useState(0)
-    const [keyword, setKeyword] = useState<string>('')
-    const [index, setIndex] = useState({machine_name: '기계명'})
-    const [subIndex, setSubIndex] = useState({tons: '정상톤'})
-    const [page, setPage] = useState<PaginationInfo>({
-        current: 1,
-    })
-    const [detailPage, setDetailPage] = useState<PaginationInfo>({
-        current: 1,
-    })
+  const [list, setList] = useState<any[]>([])
+  const [detailList, setDetailList] = useState<any[]>([])
+  const [option, setOption] = useState(0)
+  const [keyword, setKeyword] = useState<string>('')
+  const [index, setIndex] = useState({machine_name: '기계명'})
+  const [subIndex, setSubIndex] = useState({tons: '정상톤'})
+  const [page, setPage] = useState<PaginationInfo>({
+    current: 1,
+  })
+  const [detailPage, setDetailPage] = useState<PaginationInfo>({
+    current: 1,
+  })
 
-    const [selectPk, setSelectPk] = useState<any>(null)
-    const [selectMachine, setSelectMachine] = useState<any>(null)
-    const [selectValue, setSelectValue] = useState<any>(null)
+  const [selectPk, setSelectPk] = useState<any>(null)
+  const [selectMachine, setSelectMachine] = useState<any>(null)
+  const [selectValue, setSelectValue] = useState<any>(null)
 
 
-    const indexList = {
-        overtone: {
-            machine_name: '기계명',
-            manufacturer_code: '기계 번호',
-            factory_name: '공정명',
-            registered: '기계 등록 시간'
-        }
+  const indexList = {
+    overtone: {
+      machine_name: '기계명',
+      manufacturer_code: '기계 번호',
+      factory_name: '공정명',
+      registered: '기계 등록 시간'
     }
+  }
 
-    const detailTitle = {
-        overtone: {
-            tons: '정상톤',
-            overTon: '오버톤',
-            registered: '오버톤 시간',
-        },
+  const detailTitle = {
+    overtone: {
+      tons: '정상톤',
+      overTon: '오버톤',
+      registered: '오버톤 시간',
+    },
+  }
+
+  const onClick = useCallback(machine => {
+    if (machine.pk === selectPk) {
+      setSelectPk(null)
+      setSelectMachine(null)
+      setSelectValue(null)
+      setDetailPage({...detailPage, current: 1})
+    } else {
+      setSelectPk(machine.pk)
+      setSelectMachine(machine.machine_name)
+      setSelectValue(machine)
+      //TODO: api 요청
+      getData(machine.pk)
+      setDetailPage({...detailPage, current: 1})
     }
+  }, [list, selectPk])
 
-    const onClick = useCallback(machine => {
-        if (machine.pk === selectPk) {
-            setSelectPk(null)
-            setSelectMachine(null)
-            setSelectValue(null)
-            setDetailPage({...detailPage, current: 1})
-        } else {
-            setSelectPk(machine.pk)
-            setSelectMachine(machine.machine_name)
-            setSelectValue(machine)
-            //TODO: api 요청
-            getData(machine.pk)
-            setDetailPage({...detailPage, current: 1})
-        }
-    }, [list, selectPk])
+  const getData = useCallback(async (pk) => {
+    //TODO: 성공시
 
-    const getData = useCallback(async (pk) => {
-        //TODO: 성공시
+    if (pk) {
+      console.log(detailPage.current)
+      Notiflix.Loading.Circle()
+      const tempUrl = `${API_URLS['overtone'].load}?pk=${pk}&page=${detailPage.current}&limit=15`
+      const res = await getOvertoneData(tempUrl)
+      if (res) {
+        setDetailList(res.info_list)
 
-        if (pk) {
-            Notiflix.Loading.Circle()
-            const tempUrl = `${API_URLS['overtone'].load}?pk=${pk}&page=${detailPage.current}&limit=15`
-            const res = await getOvertoneData(tempUrl)
-            if (res) {
-                setDetailList(res.info_list)
-
-                setDetailPage({current: res.current_page, total: res.total_page})
-                Notiflix.Loading.Remove()
-            }
-        }
-    }, [detailList, selectPk])
+        setDetailPage({current: res.current_page, total: res.total_page})
+        Notiflix.Loading.Remove()
+      }
+    }
+  }, [detailList, selectPk, detailPage])
 
 
-    const getList = useCallback(async () => { // useCallback
-        Notiflix.Loading.Circle()
-        const tempUrl = `${API_URLS['overtone'].list}?page=${page.current}&limit=5`
-        const res = await getOvertoneData(tempUrl)
-        if (res) {
-            setList(res.info_list)
+  const getList = useCallback(async () => { // useCallback
+    Notiflix.Loading.Circle()
+    const tempUrl = `${API_URLS['overtone'].list}?page=${page.current}&limit=5`
+    const res = await getOvertoneData(tempUrl)
+    if (res) {
+      setList(res.info_list)
 
-            setPage({current: res.current_page, total: res.total_page})
-            Notiflix.Loading.Remove()
-        }
-    }, [list, page])
+      setPage({current: res.current_page, total: res.total_page})
+      Notiflix.Loading.Remove()
+    }
+  }, [list, page])
 
-    useEffect(() => {
-        setIndex(indexList['overtone'])
-        setSubIndex(detailTitle['overtone'])
-        getList()
-    }, [])
+  useEffect(() => {
+    setIndex(indexList['overtone'])
+    setSubIndex(detailTitle['overtone'])
+    getList()
+  }, [])
 
 
-    useEffect(() => {
-        getList()
-    }, [page.current])
+  useEffect(() => {
+    getList()
+  }, [page.current])
 
-    useEffect(() => {
-        getData(selectPk)
-    }, [page.current])
+  useEffect(() => {
+    getData(selectPk)
+  }, [detailPage.current])
 
-    return (
-        <OvertonTable
-            title={'프레스 오버톤'}
-            indexList={index}
-            valueList={list}
-            clickValue={selectValue}
-            currentPage={page.current}
-            totalPage={page.total}
-            pageOnClickEvent={(event, i) => setPage({...page, current: i})}
-            mainOnClickEvent={onClick}>
-            {
-                selectPk !== null ?
-                    <LineTable title={selectMachine + ' 상세내용'}
-                               contentTitle={subIndex}
-                               contentList={detailList}
-                               currentPage={detailPage.current}
-                               totalPage={detailPage.total}
-                               pageOnClickEvent={(i: number) => setDetailPage({...detailPage, current: i})}>
-                        <Line/>
-                    </LineTable>
-                    :
-                    null
-            }
-        </OvertonTable>
-    )
+  return (
+    <OvertonTable
+      title={'프레스 오버톤'}
+      indexList={index}
+      valueList={list}
+      clickValue={selectValue}
+      currentPage={page.current}
+      totalPage={page.total}
+      pageOnClickEvent={(event, i) => setPage({...page, current: i})}
+      mainOnClickEvent={onClick}>
+      {
+        selectPk !== null ?
+          <LineTable title={selectMachine + ' 상세내용'}
+                     contentTitle={subIndex}
+                     contentList={detailList}
+                     currentPage={detailPage.current}
+                     totalPage={detailPage.total}
+                     pageOnClickEvent={(i: any, v: number) => setDetailPage({...detailPage, current: v})}>
+            <Line/>
+          </LineTable>
+          :
+          null
+      }
+    </OvertonTable>
+  )
 }
 
 const Line = Styled.hr`
-    margin: 10px 20px 12px 0px;
-    border-color: #353b48;
-    height: 1px;
-    background-color: #353b48;
-`
+        margin: 10px 20px 12px 0px;
+        border-color: #353b48;
+        height: 1px;
+        background-color: #353b48;
+        `
 
 export default OvertonMaintenanceContainer
