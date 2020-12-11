f26a81d22befd3b8f78b3572366a39cccd08460e
MacBook_Pro <jmyoon555@gmail.com>
2020-12-11 11:47:18 +0900

    사용자 클릭 사라짐

--- src/Components/Navigation/ProfileBar.tsx
+++ src/Components/Navigation/ProfileBar.tsx
@@ -1,86 +1,99 @@
-import React, { useEffect , useCallback, useState} from 'react';
+import React, {useEffect, useCallback, useState} from 'react';
 import Styled from 'styled-components'
-import {BG_COLOR, BG_COLOR_SUB, SYSTEM_NAME, BG_COLOR_SUB2, COMPANY_LOGO, POINT_COLOR, MAX_WIDTH, TOKEN_NAME, BASE_URL} from '../../Common/configset'
+import {
+    BG_COLOR,
+    BG_COLOR_SUB,
+    SYSTEM_NAME,
+    BG_COLOR_SUB2,
+    COMPANY_LOGO,
+    POINT_COLOR,
+    MAX_WIDTH,
+    TOKEN_NAME,
+    BASE_URL
+} from '../../Common/configset'
 import IMG_PROFILE from '../../Assets/Images/img_profile.png'
 import IcSearch from '../../Assets/Images/ic_search_w.png'
 import IcBell from '../../Assets/Images/ic_bell_w.png'
 import NavList from './NavList'
-import { useUser, useUserDispatch } from '../../Context/UserContext';
+import {useUser, useUserDispatch} from '../../Context/UserContext';
 import moment from 'moment';
 import 'moment/locale/ko';
-import { getToken, removeToken } from '../../Common/tokenFunctions';
-import { postRequest } from '../../Common/requestFunctions';
-import { Link } from 'react-router-dom';
-import { usePopup } from '../../Context/PopupContext';
+import {getToken, removeToken} from '../../Common/tokenFunctions';
+import {postRequest} from '../../Common/requestFunctions';
+import {Link} from 'react-router-dom';
+import {usePopup} from '../../Context/PopupContext';
 
-    /*
-  useEffect(()=>{
-    const interval = setInterval(() => {
-      setNowTime(moment().format('a HH:mm'))
-    }, 100000);
+/*
+useEffect(()=>{
+const interval = setInterval(() => {
+  setNowTime(moment().format('a HH:mm'))
+}, 100000);
 
-    return () => clearInterval(interval);
+return () => clearInterval(interval);
 
-  },[nowTime])
-  */
+},[nowTime])
+*/
 
-interface Props{
-  title: string
+interface Props {
+    title: string
 }
 
 const ProfileBar = ({title}: Props) => {
 
-  const user = useUser(); // 유저 컨텍스트 데이터 받아오는 커스텀 훅스
-  const dispatch = useUserDispatch();
-  const [nowTime, setNowTime] = useState<string>(moment().format('a HH:mm'))
-  const nav = usePopup()
-  /**
-   * onClickLogout()
-   * : 로그아웃
-   * @returns X
-   */
-  const onClickLogout = useCallback(async()=>{
-    ////alert('테스트 : 로그아웃 - ' + getToken(TOKEN_NAME));
-    removeToken(TOKEN_NAME)
-    //alert('성공적으로 로그아웃 되었습니다');
-    window.location.href="/"
-    return;
-    const data = {}
-    const results = await postRequest('/logout', data, getToken(TOKEN_NAME))
+    const user = useUser(); // 유저 컨텍스트 데이터 받아오는 커스텀 훅스
+    const dispatch = useUserDispatch();
+    const [nowTime, setNowTime] = useState<string>(moment().format('a HH:mm'))
+    const nav = usePopup()
+    /**
+     * onClickLogout()
+     * : 로그아웃
+     * @returns X
+     */
+    const onClickLogout = useCallback(async () => {
+        ////alert('테스트 : 로그아웃 - ' + getToken(TOKEN_NAME));
+        removeToken(TOKEN_NAME)
+        //alert('성공적으로 로그아웃 되었습니다');
+        window.location.href = "/"
+        return;
+        const data = {}
+        const results = await postRequest('/logout', data, getToken(TOKEN_NAME))
 
-    if(results === false){
-      //TODO: 에러 처리
-    }else{
-      if(results.status === 200){
-        removeToken(TOKEN_NAME);
-        dispatch({
-          type: 'LOGOUT_USER',
-        });
-        //alert("")
-        window.location.href="/"
+        if (results === false) {
+            //TODO: 에러 처리
+        } else {
+            if (results.status === 200) {
+                removeToken(TOKEN_NAME);
+                dispatch({
+                    type: 'LOGOUT_USER',
+                });
+                //alert("")
+                window.location.href = "/"
 
-      }else if(results.status === 1001 || results.data.status === 1002){
-        //TODO:  아이디 존재 확인
-      }else{
-        //TODO:  기타 오류
-      }
-    }
-  },[])
+            } else if (results.status === 1001 || results.data.status === 1002) {
+                //TODO:  아이디 존재 확인
+            } else {
+                //TODO:  기타 오류
+            }
+        }
+    }, [])
 
 
-  return (
+    return (
 
         <SearchBarWrapDiv>
             <SearchBarInnerDiv>
-                <div style={{display:'flex', alignItems: 'center', width:'100%' }}>
-                  {/*{window.location.href.indexOf('/dashboard') !== -1 && <p  className="p-bold" ><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  대시보드 홈</p>}*/}
-                  {/*{window.location.href.indexOf('/pm') !== -1 && nav.mode === 'mes'|| window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'home' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
-                  {/*{window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'pm' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
-                  {/*{window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'mes' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
-                  {window.location.href.indexOf('/dashboard') !== -1 && <p  className="p-bold" > 대시보드 홈</p>}
-                  {window.location.href.indexOf('/pm') !== -1 && nav.mode === 'mes'|| window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'home' && <p  className="p-bold"> {title}</p>}
-                  {window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'pm' && <p  className="p-bold"> {title}</p>}
-                  {window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'mes' && <p  className="p-bold"> {title}</p>}
+                <div style={{display: 'flex', alignItems: 'center', width: '100%'}}>
+                    {/*{window.location.href.indexOf('/dashboard') !== -1 && <p  className="p-bold" ><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  대시보드 홈</p>}*/}
+                    {/*{window.location.href.indexOf('/pm') !== -1 && nav.mode === 'mes'|| window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'home' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
+                    {/*{window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'pm' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
+                    {/*{window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'mes' && <p  className="p-bold"><span style={{color:POINT_COLOR}}> {user.company_name}&nbsp;</span>  {title}</p>}*/}
+                    {window.location.href.indexOf('/dashboard') !== -1 && <p className="p-bold"> 대시보드 홈</p>}
+                    {window.location.href.indexOf('/pm') !== -1 && nav.mode === 'mes' || window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'home' &&
+                    <p className="p-bold"> {title}</p>}
+                    {window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'pm' &&
+                    <p className="p-bold"> {title}</p>}
+                    {window.location.href.indexOf('/dashboard') == -1 && nav.mode === 'mes' &&
+                    <p className="p-bold"> {title}</p>}
 
                     {/*
                   <div style={{display:'flex', alignItems: 'center', paddingLeft:10, width: '50%', height:'100%', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden',position:'relative'}}>
@@ -88,24 +101,30 @@ const ProfileBar = ({title}: Props) => {
                     <p className="p-bold p-limit" style={{color:'white', fontSize:17, display:'inline-block', fontWeight:'bold'}}>현재 데모 및 시연용 계정은 모든 관리자 기능이 해제되어있습니다</p>
                   </div>
                   */}
-                  <div style={{textAlign:'right', marginLeft:'auto', width:'70%' }}>
+                    <div style={{textAlign: 'right', marginLeft: 'auto', width: '70%'}}>
 
-                    <a className="p-eng" style={{float:'right',marginTop: 2}} onClick={onClickLogout}>
-                      Log out
-                    </a>
+                        <a className="p-eng" style={{float: 'right', marginTop: 2}} onClick={onClickLogout}>
+                            Log out
+                        </a>
 
-                    <span className="p-bold" style={{float:'right',marginRight:32, display:'inline-block', color:'white'}}>{user.name}{user.is_admin ? '(관리자)' : user.appointment}</span>
-                    <Link to="/mypage" style={{float:'right', display:'inline-block', color:'white'}}>
-                          <ImageBox style={{float:'left'}} src={user.profile_img === '' ? IMG_PROFILE : user.profile_img} />
+                        <span className="p-bold" style={{
+                            float: 'right',
+                            marginRight: 32,
+                            display: 'inline-block',
+                            color: 'white'
+                        }}>{user.name}{user.is_admin ? '(관리자)' : user.appointment}</span>
+                        <div style={{float: 'right', display: 'inline-block', color: 'white'}}>
+                            <ImageBox style={{float: 'left'}}
+                                      src={user.profile_img === '' ? IMG_PROFILE : user.profile_img}/>
 
-                    </Link>
-                  </div>
+                        </div>
+                    </div>
                 </div>
 
             </SearchBarInnerDiv>
         </SearchBarWrapDiv>
 
-  );
+    );
 }
 
 
@@ -154,9 +173,6 @@ const InputBox = Styled.input`
 
  
 `
-
-
-
 
 
 export default ProfileBar;
