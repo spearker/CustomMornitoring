2208370b591f99a07b804591512a3c314a11c8df
spearker <scchosam132@gmail.com>
2020-12-21 20:17:42 +0900

    모달 관련 이슈 해결

--- src/Components/Modal/MoldPickerModal.tsx
+++ src/Components/Modal/MoldPickerModal.tsx
@@ -9,192 +9,187 @@ import {Input} from 'semantic-ui-react'
 import IcSearchButton from '../../Assets/Images/ic_search.png'
 import {API_URLS, getSearchMachine} from '../../Api/mes/process'
 import {transferCodeToName} from '../../Common/codeTransferFunctions'
-import Pagination from "@material-ui/lab/Pagination";
+import Pagination from '@material-ui/lab/Pagination'
 import Notiflix from 'notiflix'
 
 //드롭다운 컴포넌트
 
 interface IProps {
-    select?: { name?: string, pk?: string },
-    onClickEvent: any
-    text: string
-    buttonWid?: string | number
-    disabled?: boolean
+  select?: { name?: string, pk?: string },
+  onClickEvent: any
+  text: string
+  buttonWid?: string | number
+  disabled?: boolean
 }
 
 Notiflix.Loading.Init({svgColor: '#1cb9df'})
 
 const MoldPickerModal = ({select, onClickEvent, text, buttonWid, disabled}: IProps) => {
-    //const ref = useRef() as React.MutableRefObject<HTMLInputElement>;
-    const [isOpen, setIsOpen] = useState(false)
-    const [isDisabled, setIsDisabled] = useState<boolean>(false)
-    const [machineName, setMachineName] = useState('')
-    const [page, setPage] = useState<PaginationInfo>({
-        current: 1,
-    })
+  //const ref = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const [isOpen, setIsOpen] = useState(false)
+  const [isDisabled, setIsDisabled] = useState<boolean>(false)
+  const [machineName, setMachineName] = useState('')
+  const [page, setPage] = useState<PaginationInfo>({
+    current: 1,
+  })
 
-    const [machineList, setMachineList] = useState([{
-        pk: '',
-        mold_name: '',
-        mold_type: 0,
-        limit: 0,
-        current: 0,
-        location_name: ''
-    }])
-    const [searchName, setSearchName] = useState<string>('')
+  const [machineList, setMachineList] = useState([{
+    pk: '',
+    mold_name: '',
+    mold_type: 0,
+    limit: 0,
+    current: 0,
+    location_name: ''
+  }])
+  const [searchName, setSearchName] = useState<string>('')
 
-    // const ref = useOnclickOutside(() => {
-    //     setIsOpen(false);
-    // });
+  // const ref = useOnclickOutside(() => {
+  //     setIsOpen(false);
+  // });
 
-    const getList = useCallback(async (isSearch?: boolean) => {
-        Notiflix.Loading.Circle()
-        const tempUrl = `${API_URLS['mold'].search}?keyword=${searchName}&page=${isSearch ? 1 : page.current}&limit=10`
-        const resultData = await getSearchMachine(tempUrl)
-        if (resultData) {
-            setMachineList(resultData.info_list)
-            setPage({current: resultData.current_page, total: resultData.total_page})
-        }
-        Notiflix.Loading.Remove()
-    }, [searchName])
-
-    useEffect(() => {
-        setIsDisabled(disabled ? true : false)
-    }, [disabled])
-
-
-    const handleClickBtn = () => {
-        setIsOpen(!isOpen)
+  const getList = useCallback(async (isSearch?: boolean) => {
+    Notiflix.Loading.Circle()
+    const tempUrl = `${API_URLS['mold'].search}?keyword=${searchName}&page=${isSearch ? 1 : page.current}&limit=10`
+    const resultData = await getSearchMachine(tempUrl)
+    if (resultData) {
+      setMachineList(resultData.info_list)
+      setPage({current: resultData.current_page, total: resultData.total_page})
     }
+    Notiflix.Loading.Remove()
+  }, [searchName])
 
-    useEffect(() => {
-        getList()
-    }, [page.current])
-    return (
-        <div>
-            <div style={{position: 'relative', display: 'inline-block', zIndex: 0, width: '100%'}}>
-                <BoxWrap disabled={isDisabled} onClick={() => {
-                    if (disabled) {
-                        return
-                    } else {
-                        setIsOpen(true)
-                    }
-                }} style={{padding: 0, backgroundColor: '#f4f6fa'}}>
-                    <div style={{display: 'inline-block', height: 32, width: '100%'}}>
-                        {
-                            select && select.name ? <p style={{marginTop: 5}}>&nbsp; {select.name}</p>
-                                : <p style={{marginTop: 5, color: '#b3b3b3'}}>&nbsp; {text}</p>
-                        }
+  useEffect(() => {
+    setIsDisabled(disabled ? true : false)
+  }, [disabled])
 
-                    </div>
-                    <div style={{
-                        display: 'inline-block',
-                        backgroundColor: POINT_COLOR,
-                        width: buttonWid ? buttonWid : 30,
-                        height: buttonWid ? buttonWid : 30
-                    }}>
-                        <SearchButton
-                            style={{flex: 4, width: buttonWid ? buttonWid : 30, height: buttonWid ? buttonWid : 30}}>
-                            <img src={IcSearchButton}/>
-                        </SearchButton>
-                    </div>
 
-                </BoxWrap>
+  const handleClickBtn = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    getList()
+  }, [page.current])
+  return (
+    <div>
+      <div style={{position: 'relative', display: 'inline-block', zIndex: 0, width: '100%'}}>
+        <BoxWrap disabled={isDisabled} onClick={() => {
+          if (disabled) {
+            return
+          } else {
+            setIsOpen(true)
+          }
+        }} style={{padding: 0, backgroundColor: '#f4f6fa'}}>
+          <div style={{display: 'inline-block', height: 32, width: '100%'}}>
+            {
+              select && select.name ? <p style={{marginTop: 5}}>&nbsp; {select.name}</p>
+                : <p style={{marginTop: 5, color: '#b3b3b3'}}>&nbsp; {text}</p>
+            }
+
+          </div>
+          <div style={{
+            display: 'inline-block',
+            backgroundColor: POINT_COLOR,
+            width: buttonWid ? buttonWid : 30,
+            height: buttonWid ? buttonWid : 30
+          }}>
+            <SearchButton
+              style={{flex: 4, width: buttonWid ? buttonWid : 30, height: buttonWid ? buttonWid : 30}}>
+              <img src={IcSearchButton}/>
+            </SearchButton>
+          </div>
+
+        </BoxWrap>
+      </div>
+      <Modal
+        isOpen={isOpen}
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            padding: 0
+          },
+          overlay: {
+            background: 'rgba(0,0,0,.6)',
+            zIndex: 5
+          }
+        }}
+      >
+        <div style={{width: 900}}>
+          <div style={{width: 860, minHeight: 530, maxHeight: 'auto', padding: 20}}>
+            <p style={{fontSize: 18, fontFamily: 'NotoSansCJKkr', fontWeight: 'bold'}}>• 금형 검색</p>
+            <div style={{width: 860, display: 'flex', flexDirection: 'row', marginBottom: 12}}>
+              <SearchBox placeholder="금형명을 입력해 주세요." style={{flex: 96}}
+                         onKeyPress={(event) => event.key === 'Enter' && getList(true)}
+                         onChange={(e) => setSearchName(e.target.value)}/>
+              <SearchButton style={{flex: 4}} onClick={() => getList(true)}>
+                <img src={IcSearchButton}/>
+              </SearchButton>
             </div>
-            <Modal
-                isOpen={isOpen}
-                style={{
-                    content: {
-                        top: '50%',
-                        left: '50%',
-                        right: 'auto',
-                        bottom: 'auto',
-                        marginRight: '-50%',
-                        transform: 'translate(-50%, -50%)',
-                        padding: 0
-                    },
-                    overlay: {
-                        background: 'rgba(0,0,0,.6)',
-                        zIndex: 5
-                    }
-                }}
-            >
-                <div style={{width: 900}}>
-                    <div style={{width: 860, minHeight: 530, maxHeight: 'auto', padding: 20}}>
-                        <p style={{fontSize: 18, fontFamily: 'NotoSansCJKkr', fontWeight: 'bold'}}>• 금형 검색</p>
-                        <div style={{width: 860, display: 'flex', flexDirection: 'row', marginBottom: 12}}>
-                            <SearchBox placeholder="금형명을 입력해 주세요." style={{flex: 96}}
-                                       onKeyPress={(event) => event.key === 'Enter' && getList(true)}
-                                       onChange={(e) => setSearchName(e.target.value)}/>
-                            <SearchButton style={{flex: 4}} onClick={() => getList(true)}>
-                                <img src={IcSearchButton}/>
-                            </SearchButton>
-                        </div>
-                        <div style={{
-                            minHeight: 530,
-                            maxHeight: 'auto',
-                            width: 860,
-                            backgroundColor: '#f4f6fa',
+            <div style={{minHeight: 310, maxHeight: 'auto', width: 860, backgroundColor: '#f4f6fa'}}>
+              <ReactShadowScroll>
+                <MachineTable>
+                  <tr>
+                    <th style={{width: 250}}>금형명</th>
+                    <th style={{width: 250}}>타입</th>
+                    <th style={{width: 325}}>공장명</th>
+                  </tr>
+                  {machineList !== undefined && machineList.length === 0 ?
+                    <tr>
+                      <td colSpan={4} style={{textAlign: 'center'}}>데이터가 없습니다.</td>
+                    </tr>
+                    :
+                    machineList.map((v, i) => {
+                      return (
+                        <tr style={{
+                          height: 32,
+                          backgroundColor: select ? v.pk === select.pk ? POINT_COLOR : '#ffffff' : '#ffffff',
+                        }} onClick={() => {
+                          setMachineName(v.mold_name)
+                          return onClickEvent({name: v.mold_name, pk: v.pk})
                         }}>
-                            <ReactShadowScroll>
-                                <MachineTable>
-                                    <tr>
-                                        <th style={{width: 250}}>금형명</th>
-                                        <th style={{width: 250}}>타입</th>
-                                        <th style={{width: 325}}>공장명</th>
-                                    </tr>
-                                    {machineList !== undefined && machineList.length === 0 ?
-                                        <tr>
-                                            <td colSpan={4} style={{textAlign: 'center'}}>데이터가 없습니다.</td>
-                                        </tr>
-                                        :
-                                        machineList.map((v, i) => {
-                                            return (
-                                                <tr style={{
-                                                    height: 32,
-                                                    backgroundColor: select ? v.pk === select.pk ? POINT_COLOR : '#ffffff' : '#ffffff',
-                                                }} onClick={() => {
-                                                    setMachineName(v.mold_name)
-                                                    return onClickEvent({name: v.mold_name, pk: v.pk})
-                                                }}>
-                                                    <td><span>{v.mold_name}</span></td>
-                                                    <td><span>{transferCodeToName('mold', v.mold_type)}</span></td>
-                                                    <td><span>{v.location_name}</span></td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </MachineTable>
-                            </ReactShadowScroll>
-                            <PaginationBox>
-                                <Pagination count={page.total ? page.total : 0} page={page.current}
-                                            onChange={(event, i) => setPage({...page, current: i})}
-                                            boundaryCount={1} color={'primary'}/>
-                            </PaginationBox>
-                        </div>
-                    </div>
-                    <div style={{width: 900}}>
-                        <CheckButton style={{left: 0, backgroundColor: '#e7e9eb'}} onClick={() => {
-                            onClickEvent({name: undefined, pk: undefined})
-                            setIsOpen(false)
-                        }}>
-                            <div>
-                                <span style={{color: '#666d79'}}>취소</span>
-                            </div>
-                        </CheckButton>
-                        <CheckButton style={{right: 0, backgroundColor: POINT_COLOR}} onClick={() => {
-                            setIsOpen(false)
-                        }}>
-                            <div>
-                                <span style={{color: 'black'}}>확인</span>
-                            </div>
-                        </CheckButton>
-                    </div>
-                </div>
-            </Modal>
-
+                          <td><span>{v.mold_name}</span></td>
+                          <td><span>{transferCodeToName('mold', v.mold_type)}</span></td>
+                          <td><span>{v.location_name}</span></td>
+                        </tr>
+                      )
+                    })
+                  }
+                </MachineTable>
+              </ReactShadowScroll>
+              <PaginationBox>
+                <Pagination count={page.total ? page.total : 0} page={page.current}
+                            onChange={(event, i) => setPage({...page, current: i})}
+                            boundaryCount={1} color={'primary'}/>
+              </PaginationBox>
+            </div>
+          </div>
+          <div style={{width: 900}}>
+            <CheckButton style={{left: 0, backgroundColor: '#e7e9eb'}} onClick={() => {
+              onClickEvent({name: undefined, pk: undefined})
+              setIsOpen(false)
+            }}>
+              <div>
+                <span style={{color: '#666d79'}}>취소</span>
+              </div>
+            </CheckButton>
+            <CheckButton style={{right: 0, backgroundColor: POINT_COLOR}} onClick={() => {
+              setIsOpen(false)
+            }}>
+              <div>
+                <span style={{color: 'black'}}>확인</span>
+              </div>
+            </CheckButton>
+          </div>
         </div>
-    )
+      </Modal>
+
+    </div>
+  )
 }
 
 const BoxWrap = Styled.button`
