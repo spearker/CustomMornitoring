ea0e47305dfebc87577b497a24ac417e576e17c2
MacBook_Pro <jmyoon555@gmail.com>
2020-12-22 16:25:43 +0900

    성화실업 대쉬보드 커스텀 삭제

--- src/AutoCustomSetting/autoCustomConfig.ts
+++ src/AutoCustomSetting/autoCustomConfig.ts
@@ -1,16 +1,14 @@
 import {SF_ADDRESS} from '../Api/SF_endpoint'
 
 const autoCustomType = () => {
-  switch (SF_ADDRESS.toString()) {
-    case '121.172.236.236':
-      return 'type_no_dashboard'
-    case '121.173.205.139':
-      return 'jaewoo_material_trans'
-    case '106.248.33.213':
-      return 'seain_material_trans'
-    default:
-      return ''
-  }
+    switch (SF_ADDRESS.toString()) {
+        case '121.173.205.139':
+            return 'jaewoo_material_trans'
+        case '106.248.33.213':
+            return 'seain_material_trans'
+        default:
+            return ''
+    }
 }
 
 export default autoCustomType
