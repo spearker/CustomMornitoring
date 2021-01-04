ee44931a75a46768dc594b7f3030ceb31537b316
spearker <scchosam132@gmail.com>
2021-01-04 18:38:22 +0900

    포트 수정

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,5 +1,5 @@
 export const SF_ADDRESS = `203.234.183.22`
-export const SF_ENDPOINT = `http://${SF_ADDRESS}:8022`
+export const SF_ENDPOINT = `http://${SF_ADDRESS}:8299`
 export const SF_ENDPOINT_EXCEL = `http://${SF_ADDRESS}:8399`
 export const SF_ENDPOINT_ADMIN = `http://${SF_ADDRESS}:8286/api`
 export const SF_ENDPOINT_FILE = `http://${SF_ADDRESS}:8099`
