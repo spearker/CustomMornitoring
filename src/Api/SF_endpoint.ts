a723d0be515674adea45f32629b71df845da03f6
spearker <scchosam132@gmail.com>
2021-01-11 16:27:46 +0900

    아이피수정

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -2,7 +2,7 @@ export const SF_ADDRESS = window.location.hostname
 
 const isLocalhost = () => {
   if (SF_ADDRESS === 'localhost') {
-    return '211.223.65.225'
+    return '203.234.183.22'
   } else {
     return SF_ADDRESS
   }
