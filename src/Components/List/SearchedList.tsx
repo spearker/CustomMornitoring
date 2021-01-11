ebdbc9bd5bea99091241b4d756670a0dfb119dbc
MacBook_Pro <jmyoon555@gmail.com>
2021-01-11 15:39:59 +0900

    SearchList 수정 (체크박스 삭제)

--- src/Components/List/SearchedList.tsx
+++ src/Components/List/SearchedList.tsx
@@ -4,64 +4,55 @@ import icCheck from '../../Assets/Images/ic_check_on.png'
 import icCheckDim from '../../Assets/Images/ic_check_dim.png'
 import IconSquareButton from '../Button/IconSquareButton'
 import icDelete from '../../Assets/Images/ic_minus.png'
+import {POINT_COLOR} from '../../Common/configset'
 
-interface Props{
-  pk: string,
-  contents: string[],
-  widths: string[],
-  option?: string,
-  onClickEvent :()=>void;
-  isSelected: boolean,
-  isIconDimmed: boolean,
-  type?: 'remove' | 'add'
+interface Props {
+    pk: string,
+    contents: string[],
+    widths: string[],
+    option?: string,
+    onClickEvent: () => void;
+    isSelected: boolean,
+    isIconDimmed: boolean,
+    type?: 'remove' | 'add'
 }
-
-
 
 
 // 검색결과 리스트
-const SearchedList = ({ pk , option,contents, widths, isSelected, isIconDimmed, onClickEvent, type}: Props) => {
+const SearchedList = ({pk, option, contents, widths, isSelected, isIconDimmed, onClickEvent, type}: Props) => {
 
-  useEffect(()=>{
+    useEffect(() => {
 
-  },[])
+    }, [])
 
 
+    return (
+        <ListWrapDiv style={{
+            width: type == undefined || type !== 'remove' ? 'auto' : '100%',
+            marginBottom: type == undefined || type !== 'remove' ? '0' : '11px',
+            backgroundColor: isSelected ? POINT_COLOR : 'white'
+        }} onClick={onClickEvent}>
+            <div style={{display: 'flex'}}>
 
-  return (
-    <ListWrapDiv style={{width: type == undefined || type !== 'remove' ? 'auto' : '100%',  marginBottom: type == undefined || type !== 'remove' ? '0' : '11px'}}>
-      <div style={{display:'flex'}}>
-
-      </div>
-      {
-        contents.map((v, i)=>{
-          return(
-            <div style={{ width: widths[i], color: isIconDimmed ? '#b3b3b3' : 'black'}}>
-
-              <p className="p-limit" style={{paddingLeft:8}}>{v}</p>
             </div>
-          )
-        })
-      }
-      <div onClick={onClickEvent} style={{position:'absolute', top:5, right:38, textAlign:'right'}}>
-        {option}
-      </div>
-      {
-        type == undefined || type !== 'remove' ?
-        <div onClick={onClickEvent} style={{position:'absolute', top:0, right:-1, zIndex:3}}>
-            <IconSquareButton  color="#e7e9eb" width="33px" imageSize="22px"  image={isIconDimmed? icCheckDim : icCheck} dim={isSelected ? false : true}/>
-        </div>
-        :
-        <div onClick={onClickEvent} style={{position:'absolute', top:0, right:-1}}>
-            <IconSquareButton  color="#e7e9eb" width="33px" imageSize="22px" image={icDelete} dim={true}/>
-        </div>
-      }
+            {
+                contents.map((v, i) => {
+                    return (
+                        <div style={{width: widths[i], color: isIconDimmed ? '#b3b3b3' : 'black'}}>
 
-    </ListWrapDiv>
-  );
+                            <p className="p-limit" style={{paddingLeft: 8}}>{v}</p>
+                        </div>
+                    )
+                })
+            }
+            <div onClick={onClickEvent} style={{position: 'absolute', top: 5, right: 38, textAlign: 'right'}}>
+                {option}
+            </div>
+            
+
+        </ListWrapDiv>
+    );
 }
-
-
 
 
 const ListWrapDiv = Styled.div`
@@ -74,7 +65,6 @@ const ListWrapDiv = Styled.div`
   padding-top: 6px;
   padding-bottom: 6px;
 `
-
 
 
 export default SearchedList;
