292419f5c8dfd162bb1dfce1a48ecc904f6e4c3b
spearker <scchosam132@gmail.com>
2021-01-15 13:51:35 +0900

    바코드 이미지 나오게 수정

--- src/Components/Input/RadioInput.tsx
+++ src/Components/Input/RadioInput.tsx
@@ -11,11 +11,12 @@ interface IProps {
   width?: number
   line?: boolean
   isPadding?: number
+  index?: number
 }
 
-const RadioInput = ({title, target, contents, onChangeEvent, opacity, width, line, isPadding}: IProps) => {
+const RadioInput = ({title, target, contents, onChangeEvent, opacity, width, line, isPadding, index}: IProps) => {
   useEffect(() => {
-
+    console.log(index)
   }, [])
   return (
 
@@ -25,19 +26,26 @@ const RadioInput = ({title, target, contents, onChangeEvent, opacity, width, lin
           contents.map((v, i) => {
             return (
               opacity ?
-                <div key={i} style={{opacity: target === v.value ? 1 : 0.3}}>
-                  <input type="radio" id={`rd${i}`} name="type"
+                <div key={`${i}${index}`} style={{opacity: target === v.value ? 1 : 0.3}}>
+                  <input type="radio" id={`rd${index}${i}`} name={`radio-${index}`}
                          checked={target === v.value ? true : false}
                          onClick={(e) => onChangeEvent === null ? null : onChangeEvent(v.value)}/>
-                  <label htmlFor={`rd${i}`}></label>
+                  <label htmlFor={`rd${i}${index}`}></label>
                   <span style={{paddingLeft: 4, fontSize: 14, marginRight: 20,}}>{v.title}</span>
                 </div>
                 :
-                <div key={i} style={{display: 'flex', justifyContent: 'center'}}>
-                  <input type="radio" id={`rd${i}`} name="type"
+                <div key={`${i}${index}`} style={{display: 'flex', justifyContent: 'center'}}>
+                  <input type="radio" id={`rd${index}${i}`} name={`radio-${index}`}
                          checked={target === v.value ? true : false}
-                         onClick={(e) => onChangeEvent === null ? null : onChangeEvent(v.value)}/>
-                  <label htmlFor={`rd${i}`}></label>
+                         onClick={(e) => {
+                           console.log(v)
+                           if (onChangeEvent === null) {
+                             return null
+                           } else {
+                             onChangeEvent(v.value)
+                           }
+                         }}/>
+                  <label htmlFor={`rd${index}${i}`}></label>
                   <span style={{
                     paddingLeft: 4,
                     fontSize: 14,
--- src/Components/Text/StatusTag.tsx
+++ src/Components/Text/StatusTag.tsx
@@ -1,60 +1,60 @@
-import React, {useCallback} from 'react';
+import React, {useCallback} from 'react'
 import Styled from 'styled-components'
 
 
-interface IProps{
-    text: string,
+interface IProps {
+  text: string,
 
 }
 
 const StatusTag = ({text}: IProps) => {
 
 
-    const changeStatusToString = useCallback((status: string)=>{
-        if(status === 'active'){
-            return '진행'
-        }else if(status === 'done'){
-            return '완료'
-        }else if(status === 'stop'){
-            return '중지'
-        }else if(status === 'share'){
-            return '공유'
-        }else if(status === 'ready'){
-            return '대기'
-        }else if(status === 'off'){
-            return '꺼짐'
-        } else if(status === 'error'){
-            return '에러'
-        }else if(status ==='reservation'){
-            return '예약'
-        }else{
-            return '대기'
-        }
+  const changeStatusToString = useCallback((status: string) => {
+    if (status === 'active') {
+      return '진행'
+    } else if (status === 'done') {
+      return '완료'
+    } else if (status === 'stop') {
+      return '중지'
+    } else if (status === 'share') {
+      return '공유'
+    } else if (status === 'ready') {
+      return '대기'
+    } else if (status === 'off') {
+      return '계획정지'
+    } else if (status === 'error') {
+      return '에러'
+    } else if (status === 'reservation') {
+      return '예약'
+    } else {
+      return '대기'
+    }
 
-    },[])
+  }, [])
 
-    const changeStatusToColor = useCallback((status: string)=>{
-        if(status === 'active'){
-            return '#25b4b4'
-        }else if(status === 'done'){
-            return '#2760ff'
-        }else if(status === 'stop'){
-            return '#fd6b00'
-        }else if(status === 'error'){
-            return '#ff461a'
-        }else if(status === 'share'){
-            return '#683be5'
-        }else if(status === 'ready'){
-            return '#717c90'
-        }else if(status === 'reservation'){
-            return '#f8a506'
-        }else{
-            return '#717c90'
-        }
+  const changeStatusToColor = useCallback((status: string) => {
+    if (status === 'active') {
+      return '#25b4b4'
+    } else if (status === 'done') {
+      return '#2760ff'
+    } else if (status === 'stop') {
+      return '#fd6b00'
+    } else if (status === 'error') {
+      return '#ff461a'
+    } else if (status === 'share') {
+      return '#683be5'
+    } else if (status === 'ready') {
+      return '#717c90'
+    } else if (status === 'reservation') {
+      return '#f8a506'
+    } else {
+      return '#717c90'
+    }
 
-    },[])
+  }, [])
 
-    const BoxWrap = Styled.div`
+  const BoxWrap = Styled.div`
         background-color: ${changeStatusToColor(text)};
         padding-top: 10px;
         padding-bottom: 10px;
@@ -71,13 +71,13 @@ const StatusTag = ({text}: IProps) => {
 
   return (
 
-        <BoxWrap>
-            <p className="p-bold" style={{width:'100%', display:'block', textAlign:'center'}} >{changeStatusToString(text)}</p>
-        </BoxWrap>
+    <BoxWrap>
+      <p className="p-bold"
+         style={{width: '100%', display: 'block', textAlign: 'center'}}>{changeStatusToString(text)}</p>
+    </BoxWrap>
 
-  );
+  )
 }
 
 
-
-export default StatusTag;
+export default StatusTag
