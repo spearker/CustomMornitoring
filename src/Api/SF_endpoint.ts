29f64c0fb13a4531ea76a46bab7ed9f0b755da53
MacBook_Pro <jmyoon555@gmail.com>
2021-01-16 18:53:30 +0900

    엔드포인트 마지막 수정

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -2,7 +2,7 @@ export const SF_ADDRESS = window.location.hostname
 
 export const isLocalhost = () => {
     if (SF_ADDRESS === 'localhost') {
-        return '106.248.33.213'
+        return '203.234.183.22'
     } else {
         return SF_ADDRESS
     }
