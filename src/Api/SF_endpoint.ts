e19a27399420cc9e1fde57be13fe52ad90d84ab0
spearker <scchosam132@gmail.com>
2021-01-19 21:02:33 +0900

    수정: SF_ENDPOINT수정

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,7 +1,7 @@
 export const SF_ADDRESS = window.location.hostname
 
 export const isLocalhost = () => {
-  if (SF_ADDRESS === 'localhost' || SF_ADDRESS === '61.101.55.224') {
+  if (SF_ADDRESS === 'localhost') {
     return '203.234.183.22'
   } else {
     return SF_ADDRESS
