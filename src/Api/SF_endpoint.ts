4ead90d0f8877543e689b855ae91fe8e08480987
spearker <scchosam132@gmail.com>
2021-01-20 19:40:02 +0900

    merget

--- src/Api/SF_endpoint.ts
+++ src/Api/SF_endpoint.ts
@@ -1,14 +1,14 @@
 export const SF_ADDRESS = window.location.hostname
 
 export const isLocalhost = () => {
-    if (SF_ADDRESS === 'localhost') {
-        return '203.234.183.22'
-    } else {
-        return SF_ADDRESS
-    }
+  if (SF_ADDRESS === 'localhost') {
+    return '61.101.55.224'
+  } else {
+    return SF_ADDRESS
+  }
 }
 
-export const SF_ENDPOINT = `http://${isLocalhost()}:8299`
+export const SF_ENDPOINT = `http://${isLocalhost()}:9912`
 export const SF_ENDPOINT_EXCEL = `http://${isLocalhost()}:8399`
 export const SF_ENDPOINT_ADMIN = `http://${isLocalhost()}:8286/api`
 export const SF_ENDPOINT_FILE = `http://${isLocalhost()}:8099`
--- src/Components/List/NavGroupList.tsx
+++ src/Components/List/NavGroupList.tsx
@@ -6,96 +6,101 @@ import {useHistory} from 'react-router-dom'
 
 //대시보드 네비게이션 리스트
 interface Props {
-    contents: { name: string, url: string }[]
-    selected: boolean
-    onClickEvent: any
-    onClickMode?: any
+  contents: { name: string, url: string }[]
+  selected: boolean
+  onClickEvent: any
+  onClickMode?: any
 }
 
 
 const NavGroupList = ({contents, selected, onClickMode, onClickEvent}: Props) => {
 
-    const history = useHistory()
-    useEffect(() => {
+  const history = useHistory()
+  useEffect(() => {
 
-    }, [])
+  }, [])
 
-    return (
-        <div>
+  return (
+    <div>
+      {
+        contents.length !== 0 &&
+        <>
             <ListDiv>
                 <a onClick={onClickEvent}>{contents[0].name}</a>
             </ListDiv>
-            {
-                selected ?
-                    contents.map((v, i) => {
-                        if (i === 0) {
-                            return
+          {
+            selected ?
+              contents.map((v, i) => {
+                if (i === 0) {
+                  return
+                } else {
+                  return (
+                    <ListInnderDiv key={`list-${i}`} onClick={onClickMode}>
+                      <p onClick={() => {
+                        window.scrollTo(0, 0)
+                        if (v.name === '전력' || v.name === '오일 공급') {
+                          window.location.href = v.url
                         } else {
-                            return (
-                                <ListInnderDiv key={`list-${i}`} onClick={onClickMode}>
-                                    <p onClick={() => {
-                                        window.scrollTo(0, 0)
-                                        if (v.name === '전력' || v.name === '오일 공급') {
-                                            window.location.href = v.url
-                                        } else {
-                                            history.push(v.url)
-                                        }
-                                    }}><span>· </span>{v.name}</p>
-                                </ListInnderDiv>
-
-                            )
+                          history.push(v.url)
                         }
-                    })
-                    :
-                    null
-            }
-        </div>
-    )
+                      }}><span>· </span>{v.name}</p>
+                    </ListInnderDiv>
+
+                  )
+                }
+              })
+              :
+              null
+          }
+        </>
+      }
+    </div>
+  )
 }
 
 
 const ListInnderDiv = Styled.div`
-  text-align: left;
-  font-size: 16px;
-  color: white;
-  p, a{
-    display: block;
-    margin-left: 34px;
-    padding-top: 13px;
-    padding-bottom: 11px;
-  }
-  &:hover{
-    background-color: ${BG_COLOR_SUB3}60;
-  }
-  span{
-    color: #cccccc;
-    font-weight: bold;
-  }
-  cursor: pointer;
-  color: #eeeeee;
-  
-`
+        text-align: left;
+        font-size: 16px;
+        color: white;
+        p, a{
+        display: block;
+        margin-left: 34px;
+        padding-top: 13px;
+        padding-bottom: 11px;
+        }
+        &:hover{
+        background-color: ${BG_COLOR_SUB3}60;
+        }
+        span{
+        color: #cccccc;
+        font-weight: bold;
+        }
+        cursor: pointer;
+        color: #eeeeee;
+
+        `
 
 
 const ListDiv = Styled.div`
 
-  
-  text-align: left;
-  font-size: 16px;
-  color: #eeeeee;
-  p, a{
-    display: block;
-    margin-left: 27px;
-    padding-top: 13px;
-    padding-bottom: 11px;
-    border-bottom: 1.3px solid ${BG_COLOR_SUB3};
-  }
-  cursor: pointer;
-  &:hover{
 
-    background-color: ${BG_COLOR_SUB3};
-  }
-`
+        text-align: left;
+        font-size: 16px;
+        color: #eeeeee;
+        p, a{
+        display: block;
+        margin-left: 27px;
+        padding-top: 13px;
+        padding-bottom: 11px;
+        border-bottom: 1.3px solid ${BG_COLOR_SUB3};
+        }
+        cursor: pointer;
+        &:hover{
+
+        background-color: ${BG_COLOR_SUB3};
+        }
+        `
 
 
 export default NavGroupList
